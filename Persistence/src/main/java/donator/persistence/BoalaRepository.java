878f76e99faa39312542f376c8af5da631143d70
balanpaul <balanpaul1@yahoo.com>
2018-05-22 12:49:43 +0300

    persistence create

---
+++ Persistence/src/main/java/donator/persistence/BoalaRepository.java
@@ -0,0 +1,32 @@
+package donator.persistence;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BoalaRepository {
+    private SessionFactory sessionFactory;
+
+    public BoalaRepository() {
+        try{
+            initialize();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    private void initialize() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+        try {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        } catch (Exception e) {
+            StandardServiceRegistryBuilder.destroy(registry);
+            e.printStackTrace();
+        }
+    }
+}
