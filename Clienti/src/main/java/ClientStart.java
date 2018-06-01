716a83c34494e6cd34ca48b2b4599dbfd2b6b351
balanpaul <balanpaul1@yahoo.com>
2018-06-01 17:37:39 +0300

    server-client

---
+++ Clienti/src/main/java/ClientStart.java
@@ -0,0 +1,25 @@
+import donator.service.IServer;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class ClientStart extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+
+            ApplicationContext factory = new ClassPathXmlApplicationContext("classpath:spring-client.xml");
+            IServer server;
+            server = (IServer) factory.getBean("service");
+        } catch (Exception e) {
+            System.err.println("Initialization  exception:" + e);
+            e.printStackTrace();
+        }
+    }
+}
--- Server/src/main/java/donator/server/ServerStart.java
+++ Server/src/main/java/donator/server/ServerStart.java
@@ -1,4 +1,9 @@
 package donator.server;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class ServerStart {
+    ApplicationContext factory= new ClassPathXmlApplicationContext("classpath:spring-server.xml");
+
 }
